2866ed5178619830ecefe8a689c14dfef956ca7d
Brantley Burton <brantleyb15@gmail.com>
2018-08-18 00:15:30 -0400

    fixed some css

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ class App extends React.Component {
     return (
       <div className='section'>
         <h1 className='title'>Find the Tee</h1>
+        <h2></h2>
+        <h2 className='title'>For all your golf course searching needs </h2>
         <div className='columns'>
           <Courses />
         </div>
