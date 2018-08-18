3bf74d7e3ec01c09f5e589e02662f681e2b10eb5
Brantley Burton <brantleyb15@gmail.com>
2018-08-17 20:27:42 -0400

    added user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   post '/courses', to: 'courses#create'
   delete '/courses/:id', to: 'courses#delete'
   put '/courses/:id', to: 'courses#update'
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#delete'
+  put '/users/:id', to: 'users#update'
 end
