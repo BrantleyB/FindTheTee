1034fe4a0adbbd241db87eb92b81a8523703b2c5
Brantley Burton <brantleyb15@gmail.com>
2018-08-17 20:26:33 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/courses', to: 'courses#index'
+  get '/courses/:id', to: 'courses#show'
+  post '/courses', to: 'courses#create'
+  delete '/courses/:id', to: 'courses#delete'
+  put '/courses/:id', to: 'courses#update'
 end
